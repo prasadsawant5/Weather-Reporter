c346b6facb9659e33b9f9fc420073186db4fef8c
Prasad Sawant <prasadsawant@prasads-macbook-pro.local>
2016-01-21 00:07:07 -0800

    Initial Commit

---
+++ Weather Reporter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather Reporter
+//
+//  Created by Prasad Sawant on 1/21/16.
+//  Copyright © 2016 Prasad Sawant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
